932e6b584e04d28d820157e6b6b61499ee37e943
Marco Antonio Uscamayta S. <maus279000@gmail.com>
2026-04-01 12:59:54 -0900

    modificaciones v1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -63,7 +63,7 @@ const Header = ({
   return (
     <header className="encabezado-aplicacion">
       <div className="izquierda-encabezado">
-        <button 
+        <button
           className="alternar-sidebar"
           onClick={() => setSidebarAbierto(!sidebarAbierto)}
           title={sidebarAbierto ? "Ocultar sidebar" : "Mostrar sidebar"}
@@ -77,14 +77,14 @@ const Header = ({
       </div>
       <div className="derecha-encabezado">
         {/* Menú de control de paneles */}
-        <PanelControlMenu 
+        <PanelControlMenu
           panelesVisibles={panelesVisibles}
           togglePanel={togglePanel}
           restablecerDisposicion={restablecerDisposicion}
           setPanelesVisibles={setPanelesVisibles} // Pasar la nueva prop
         />
 
-        <button 
+        <button
           className="icono-encabezado"
           onClick={abrirMessenger}
           title="Abrir Messenger"
@@ -96,7 +96,7 @@ const Header = ({
         {/* Componente Notifications integrado */}
         <Notifications />
 
-        <button 
+        <button
           className={`icono-encabezado ${rightSidebarAbierto ? 'activo' : ''}`}
           onClick={() => setRightSidebarAbierto(!rightSidebarAbierto)}
           title={rightSidebarAbierto ? "Ocultar herramientas" : "Mostrar herramientas"}
